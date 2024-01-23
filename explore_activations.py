a6c9aaeaa49d1b0416dba58d10667ef05e1e5efb
Noah MacCallum <noahmacca@gmail.com>
2024-01-23 15:42:43 +0000

    Refactor, save outputs

--- explore_activations.py
+++ explore_activations.py
@@ -5,10 +5,16 @@ import seaborn as sns
 import pandas as pd
 import os
 from PIL import Image
+from datetime import datetime
+
 
 # Load activations
 act_path = "./data/activations"
 img_path = "./data/images"
+
+# For saving output figs
+fig_dir = "outputs/"
+os.makedirs(fig_dir, exist_ok=True)
 
 # List all files in the path directory with .parquet extension
 parquet_files = [f for f in os.listdir(act_path) if f.endswith(".parquet")]
@@ -53,52 +59,72 @@ dft2.head()
 
 # %%
 # For the most highly activating neurons, look at activations on those images
-ref_row_top_act = dft2.iloc[15]
-ref_df_all_act = df_act[
-    (df_act["neuron_idx"] == ref_row_top_act["neuron_idx"])
-    & (df_act["batch_idx"] == ref_row_top_act["batch_idx"])
-]
-
-info = """
-neuron_idx={}
-class_name={}
-predicted={}
-mean max activation for class={}
-img batch_idx={}
-""".format(
-    ref_row_top_act["neuron_idx"],
-    ref_row_top_act["class_name"],
-    ref_df_all_act["predicted"].iloc[0],
-    round(ref_row_top_act["mean"], 5),
-    ref_df_all_act["batch_idx"].iloc[0],
-)
 
 
-# get activations
-act_i = np.array(ref_df_all_act["activation_value"])[1:].reshape(14, 14)
+def plot_neuron_act_for_img(neuron_idx, img_idx, ax_in=None):
+    ref_df_all_act = df_act[
+        (df_act["neuron_idx"] == neuron_idx) & (df_act["batch_idx"] == img_idx)
+    ]
 
-scaled = np.zeros((224, 224))
-pixels_per_cell = 16
-for i in range(act_i.shape[0]):
-    for j in range(act_i.shape[1]):
-        scaled[
-            i * pixels_per_cell : (i + 1) * pixels_per_cell,
-            j * pixels_per_cell : (j + 1) * pixels_per_cell,
-        ] = act_i[i][j]
-
-# for i, ax in enumerate(axs.flatten()):
-img = Image.open(
-    os.path.join(
-        img_path, batch_idx_to_img_file_name[ref_df_all_act["batch_idx"].iloc[0]]
+    info = """
+    neuron_idx={}
+    class_name={}
+    predicted={}
+    img batch_idx={}
+    """.format(
+        neuron_idx,
+        ref_df_all_act["class_name"].iloc[0],
+        ref_df_all_act["predicted"].iloc[0],
+        ref_df_all_act["batch_idx"].iloc[0],
     )
-)
 
-f, ax = plt.subplots(1, 1)
-ax.imshow(img)
-ax.imshow(scaled, alpha=0.5)
-ax.axis("off")
-ax.set_title(info)
+    # get activations and scale them to match dims of image
+    act_i = np.array(ref_df_all_act["activation_value"])[1:].reshape(14, 14)
 
+    # There's probably a built-in way of doing this
+    scaled = np.zeros((224, 224))
+    pixels_per_cell = 16
+    for i in range(act_i.shape[0]):
+        for j in range(act_i.shape[1]):
+            scaled[
+                i * pixels_per_cell : (i + 1) * pixels_per_cell,
+                j * pixels_per_cell : (j + 1) * pixels_per_cell,
+            ] = act_i[i][j]
+
+    # for i, ax in enumerate(axs.flatten()):
+    img = Image.open(
+        os.path.join(
+            img_path, batch_idx_to_img_file_name[ref_df_all_act["batch_idx"].iloc[0]]
+        )
+    )
+
+    if ax_in == None:
+        f, ax = plt.subplots(1, 1)
+        ax.imshow(img)
+        ax.imshow(scaled, alpha=0.5)
+        ax.axis("off")
+        ax.set_title(info)
+    else:
+        ax_in.imshow(img)
+        ax_in.imshow(scaled, alpha=0.5)
+        ax_in.axis("off")
+        ax_in.set_title(info)
+
+
+# %%
+# Plot the overall top activating neuron-image pairs
+f, axs = plt.subplots(4, 4, figsize=(16, 16))
+for i, ax in enumerate(axs.flatten()):
+    ref_row_top_act = dft2.iloc[i]
+    plot_neuron_act_for_img(
+        ref_row_top_act["neuron_idx"], ref_row_top_act["batch_idx"], ax_in=ax
+    )
+
+f.tight_layout()
+
+timestamp = datetime.now().strftime("%b %d %Y %H:%M:%S")
+filename = os.path.join(fig_dir, f"top_activations_{timestamp}.png")
+plt.savefig(filename)
 
 # %%
 # Load the images and display them alongside their activations
