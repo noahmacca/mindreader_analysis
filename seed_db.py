75b3bb4896b56cd37d0dbfdde3bf9ff04ee89c56
Noah MacCallum <noahmacca@gmail.com>
2024-01-23 20:16:53 +0000

    Separate create and seed db logic

--- seed_db.py
+++ seed_db.py
@@ -1,86 +1,69 @@
 # %%
-from dotenv import load_dotenv
 import os
-from sqlalchemy import (
-    create_engine,
-    Column,
-    Integer,
-    Float,
-    String,
-    ForeignKey,
-    Table,
-    LargeBinary,
-)
-from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy.dialects.postgresql import ARRAY
+import pandas as pd
 
-load_dotenv()
+# Load images
+image_path = "./data/images"
+image_files = [f for f in os.listdir(image_path) if f.endswith(".jpeg")]
 
-engine = create_engine(os.getenv("DB_CONN_LOCAL"))
+image_data = []
+for image_file in image_files:
+    with open(os.path.join(image_path, image_file), "rb") as file:
+        binary_data = file.read()
+        id = int(image_file.split("new_index_")[1].split(".")[0])
+        label = image_file.split("_orig")[0]
+        image_data.append({"id": id, "label": label, "data": binary_data})
 
-Base = declarative_base()
+df_image = pd.DataFrame(image_data)
 
+# Load activations
+act_path = "./data/activations"
 
-class Image(Base):
-    __tablename__ = "images"
-    id = Column(Integer, primary_key=True)
-    label = Column(String)
-    predicted = Column(String)
-    max_activation = Column(Float)
-    data = Column(LargeBinary)
+# List all files in the path directory with .parquet extension
+parquet_files = [f for f in os.listdir(act_path) if f.endswith(".parquet")]
+print("Parquet files found:", parquet_files)
 
 
-class Neuron(Base):
-    __tablename__ = "neurons"
-    id = Column(String, primary_key=True)
-    description = Column(String)
-    max_activation = Column(Float)
+layer_filename_to_id_prefix = {
+    "Activations FC1 7.parquet": "7_FC1_",
+    "Activations FC2 7.parquet": "7_FC2_",
+    "Activations 7.parquet": "7_",
+}
 
+activation_dfs = []
 
-class NeuronImageActivation(Base):
-    __tablename__ = "neuron_image_activations"
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    neuron_id = Column(String, ForeignKey("neurons.id"))
-    image_id = Column(Integer, ForeignKey("images.id"))
-    patch_activations = Column(ARRAY(Float))
+for file in [i for i in parquet_files if "FC" in i]:
+    # Just the FC layers for now
+    print("loading {}...".format(file))
+    df_i = pd.read_parquet(os.path.join(act_path, file))
+    df_i["neuron_id"] = df_i["neuron_idx"].apply(
+        lambda x: "{}{}".format(layer_filename_to_id_prefix[file], x)
+    )
+    activation_dfs.append(df_i)
 
-
-# Drop existing tables
-Base.metadata.drop_all(engine)
-
-# Create the tables in the database
-Base.metadata.create_all(engine)
+df_act = pd.concat(activation_dfs, axis=0)
+df_act.rename(columns={"batch_idx": "img_idx"}, inplace=True)
 
 # %%
-with open("data/images/academic_gown_orig_index_400_new_index_412.jpeg", "rb") as file:
-    binary_data = file.read()
-
-# Create a session
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# Example data to append
-image_data = Image(
-    id=1,
-    label="basset hound",
-    predicted="basset hound",
-    max_activation=3.1,
-    data=binary_data,
+# Append necessary fields onto df_image
+# Create a dictionary with img_idx as keys and unique predicted values as values
+img_idx_to_predicted = (
+    df_act.drop_duplicates(subset="img_idx").set_index("img_idx")["predicted"].to_dict()
 )
-neuron_data = Neuron(
-    id="7_1_211", description="fires on dogs, faces, bugs", max_activation=3.2
+df_image["predicted"] = df_image["id"].apply(lambda x: img_idx_to_predicted[x])
+df_image.head()
+
+# Get image max activation value
+df_act.head()
+
+img_idx_to_max_activation = (
+    df_act.groupby(["img_idx"])["activation_value"].max().to_dict()
 )
-neuron_image_activation_data = NeuronImageActivation(
-    neuron_id="7_1_211", image_id=1, patch_activations=[1, 0, 0, 122, 255]
+df_image["max_activation"] = df_image["id"].apply(
+    lambda x: img_idx_to_max_activation[x]
 )
 
-# Add data to the session
-session.add(image_data)
-session.add(neuron_data)
-session.add(neuron_image_activation_data)
+df_image.head()
 
-# Commit the transaction
-session.commit()
-
-# Close the session
-session.close()
+# %%
+# Write to the image table
