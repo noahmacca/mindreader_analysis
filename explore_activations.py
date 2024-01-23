e9cf9316842f0a63267d8d1602ef00490ed74c29
Noah MacCallum <noahmacca@gmail.com>
2024-01-23 16:26:28 +0000

    Refactor

--- explore_activations.py
+++ explore_activations.py
@@ -28,54 +28,35 @@ def load_parquet_file(file_path):
 
 df_act = load_parquet_file(os.path.join(act_path, parquet_files[0]))
 print("loaded {} rows from {}".format(len(df_act), parquet_files[0]))
+df_act.rename(columns={"batch_idx": "img_idx"}, inplace=True)
 
 # Get image paths
 image_files = [f for f in os.listdir(img_path) if f.endswith(".jpeg")]
 
 # create map with batch_idx as the key
-batch_idx_to_img_file_name = {}
+img_idx_to_img_file_name = {}
 for img in image_files:
     idx = int(img.split("new_index_")[1].split(".")[0])
-    batch_idx_to_img_file_name[idx] = img
-
-# %%
-dft = df_act.groupby(["batch_idx", "class_name", "neuron_idx"])["activation_value"].agg(
-    ["mean", "max", "count"]
-)
-dft.reset_index(inplace=True)
-dft.sort_values(by=["neuron_idx", "max"], ascending=[True, False])
-
-# %%
-dft1 = (
-    dft.groupby(["neuron_idx", "batch_idx", "class_name"])["max"]
-    .agg(["mean", "sum"])
-    .sort_values(by=["neuron_idx", "sum"], ascending=False)
-    .reset_index()
-)
-dft2 = dft1.groupby(["neuron_idx"]).first().reset_index()
-dft2.sort_values(by="mean", ascending=False, inplace=True)
-dft2.head()
+    img_idx_to_img_file_name[idx] = img
 
 
 # %%
-# For the most highly activating neurons, look at activations on those images
-
-
+# display any neuron img pair
 def plot_neuron_act_for_img(neuron_idx, img_idx, ax_in=None):
     ref_df_all_act = df_act[
-        (df_act["neuron_idx"] == neuron_idx) & (df_act["batch_idx"] == img_idx)
+        (df_act["neuron_idx"] == neuron_idx) & (df_act["img_idx"] == img_idx)
     ]
 
     info = """
     neuron_idx={}
     class_name={}
     predicted={}
-    img batch_idx={}
+    img_idx={}
     """.format(
         neuron_idx,
         ref_df_all_act["class_name"].iloc[0],
         ref_df_all_act["predicted"].iloc[0],
-        ref_df_all_act["batch_idx"].iloc[0],
+        ref_df_all_act["img_idx"].iloc[0],
     )
 
     # get activations and scale them to match dims of image
@@ -94,7 +75,7 @@ def plot_neuron_act_for_img(neuron_idx, img_idx, ax_in=None):
     # for i, ax in enumerate(axs.flatten()):
     img = Image.open(
         os.path.join(
-            img_path, batch_idx_to_img_file_name[ref_df_all_act["batch_idx"].iloc[0]]
+            img_path, img_idx_to_img_file_name[ref_df_all_act["img_idx"].iloc[0]]
         )
     )
 
@@ -112,35 +93,44 @@ def plot_neuron_act_for_img(neuron_idx, img_idx, ax_in=None):
 
 
 # %%
-# Plot the overall top activating neuron-image pairs
-f, axs = plt.subplots(4, 4, figsize=(16, 16))
+# Get the most highly activating neuron-image pairs, as defined by the max activating
+# patch for a given neuron anywhere on a certain image.
+df_top_neuron_image_acivations = (
+    df_act.groupby(["img_idx", "class_name", "neuron_idx"])["activation_value"]
+    .agg(["mean", "max"])
+    .reset_index()
+    .sort_values(by=["neuron_idx", "max"], ascending=[True, False])
+)
+df_top_neuron_image_acivations.head()
+
+f, axs = plt.subplots(8, 8, figsize=(32, 32))
 for i, ax in enumerate(axs.flatten()):
-    ref_row_top_act = dft2.iloc[i]
+    ref_row_top_act = df_top_neuron_image_acivations.iloc[i]
     plot_neuron_act_for_img(
-        ref_row_top_act["neuron_idx"], ref_row_top_act["batch_idx"], ax_in=ax
+        ref_row_top_act["neuron_idx"], ref_row_top_act["img_idx"], ax_in=ax
     )
 
 f.tight_layout()
 
-timestamp = datetime.now().strftime("%b %d %Y %H:%M:%S")
+# save
+timestamp = datetime.now().strftime("%b %d %Y %H-%M-%S")
 filename = os.path.join(fig_dir, f"top_activations_{timestamp}.png")
 plt.savefig(filename)
+
 
 # %%
 # Load the images and display them alongside their activations
 
 df_act.head()
-dft = (
+df_top_neuron_image_acivations = (
     df_act[df_act["neuron_idx"] == 0]
     .sort_values(by="activation_value", ascending=False)
-    .drop_duplicates(subset="batch_idx")
+    .drop_duplicates(subset="img_idx")
 )
 fig, axs = plt.subplots(4, 4, figsize=(12, 12))
 for i, ax in enumerate(axs.flatten()):
-    row = dft.iloc[i]
-    img = Image.open(
-        os.path.join(img_path, batch_idx_to_img_file_name[row["batch_idx"]])
-    )
+    row = df_top_neuron_image_acivations.iloc[i]
+    img = Image.open(os.path.join(img_path, img_idx_to_img_file_name[row["img_idx"]]))
     ax.imshow(img)
     ax.axis("off")
     ax.set_title("label={}\npredicted={}".format(row["class_name"], row["predicted"]))
