58666acc76fc8a26d0b5fdf18c4fd9c1c0acde7b
Noah MacCallum <noahmacca@gmail.com>
2024-04-26 12:19:36 -1000

    update autointerp label calculation code

--- new_outputs_3_28/explore2.py
+++ new_outputs_3_28/explore2.py
@@ -1,13 +1,7 @@
 # %%
-
 import pandas as pd
+import json
 
-df = pd.read_parquet(
-    "./data/imagenet_1k_patch_labels/combined_final/final_layer_num_7_dataset_fold_train.parquet"
-)
-df.groupby(["unique_index", "neuron_index", "type"])["index"].count()
-
-# %%
 # Load the patch activations
 MODEL = "CLIP_TINY"
 FEATURE_TYPE = "NEURON"
@@ -27,7 +21,7 @@ def make_feature_id(model, feature_type, layer_index, layer_type, feature_index)
 
 
 dfs = []
-for layerIdx in range(6, 8):
+for layerIdx in range(0, 10):
     df_i = pd.read_parquet(
         "./data/imagenet_1k_patch_labels/combined_final/final_layer_num_{}_dataset_fold_train.parquet".format(
             layerIdx
@@ -67,6 +61,8 @@ df.head()
 # %%
 # Make the featureImageActivation table
 
+TOP_N_FEAT_PER_LAYER = 50
+
 # Calculate z score using the "random" values
 df_stats_random = (
     df[df["type"] == "random"]
@@ -94,15 +90,17 @@ df_top_features = (
 df_top_features["feature_rank"] = df_top_features.groupby(["layerIdx"])[
     "activationValue"
 ].rank(method="dense", ascending=False)
-df_top_features = df_top_features[df_top_features["feature_rank"] <= 10]
+df_top_features = df_top_features[
+    df_top_features["feature_rank"] <= TOP_N_FEAT_PER_LAYER
+]
 df_top_features.drop(columns=["feature_rank", "activationValue"], inplace=True)
 
-# Merge df_top with df_top_images to keep only the top 10 images per top neuron
+# Merge df_top with df_top_images to keep only the top TOP_N_FEAT_PER_LAYER images per top neuron
 df_top = df_top.merge(df_top_features, on=["layerIdx", "featureIdx"])
 
 print(len(df_top))
 print(
-    f"Only top 10 features. Down to {len(df_top)} rows, using {df_top.memory_usage(deep=True).sum() / 1024**2:.2f} MB in memory"
+    f"Only top {TOP_N_FEAT_PER_LAYER} features. Down to {len(df_top)} rows, using {df_top.memory_usage(deep=True).sum() / 1024**2:.2f} MB in memory"
 )
 
 # Write to disk
@@ -156,48 +154,74 @@ df_feat_hist.rename(columns={"activationValue": "activationHistVals"}, inplace=T
 
 df_feat = df_feat.merge(df_feat_hist, on="featureId")
 
-df_feat.head(20)
+# %%
+# Calculate autointerp strings and statistics
+df_autointerp_labels = df_top[df_top["activationZScore"] > 1.0].copy()
+
+label_string_to_list = lambda x: (
+    [] if x == "None" else [i.strip().lower() for i in x.split(",")]
+)
+df_autointerp_labels["label_list"] = df_autointerp_labels["label"].apply(
+    label_string_to_list
+)
+
+df_autointerp_labels["label_list_weighted"] = df_autointerp_labels.apply(
+    lambda x: [(i, round(x["activationZScore"], 4)) for i in x["label_list"]], axis=1
+)
+
+# Calculate score by count * average zScore for each label
+df_autointerp_labels = (
+    df_autointerp_labels.groupby(["featureId"])["label_list_weighted"]
+    .agg("sum")
+    .apply(
+        lambda x: {
+            label: (
+                len([label_ for label_, _ in x if label_ == label]),
+                int(sum(weight for label_, weight in x if label_ == label)),
+            )
+            for label in set(label for label, _ in x)
+        }
+    )
+    .apply(
+        lambda label_counts: sorted(
+            [
+                (label, count * avg_activation)
+                for label, (count, avg_activation) in label_counts.items()
+            ],
+            key=lambda item: item[1],
+            reverse=True,
+        )
+    )
+    .reset_index(name="concat_labels")
+)
+
+# Calculate the gini coefficient for each set of concat_labels
+df_autointerp_labels["autointerpScoreGini"] = df_autointerp_labels[
+    "concat_labels"
+].apply(
+    lambda labels: 1
+    - sum((count / sum(count for _, count in labels)) ** 2 for _, count in labels)
+)
+
+df_autointerp_labels["autoInterp"] = df_autointerp_labels["concat_labels"].apply(
+    lambda x: json.dumps(x[:7])
+)
+
+df_autointerp_labels["autointerpScoreMax"] = df_autointerp_labels[
+    "concat_labels"
+].apply(lambda x: x[0][1])
+
+df_autointerp_labels.drop(columns=["concat_labels"], inplace=True)
+
+df_autointerp_labels.head()
+
+# %%
+# Add autointerp feature fields and write to disk
+df_feat = df_feat.merge(df_autointerp_labels, on="featureId", how="left")
 
 # Write to disk
 df_feat.to_csv(os.path.join(DB_FILES_PATH, "feature.csv"), index=False)
 
+df_feat.head()
 
 # %%
-from tqdm import tqdm
-
-tqdm.pandas()
-
-df["feature_id"] = df.progress_apply(
-    lambda x: make_feature_id(
-        MODEL, FEATURE_TYPE, x["layer_index"], LAYER_TYPE, x["feature_index"]
-    ),
-    axis=1,
-)
-
-df.head()
-
-# %%
-df.head()
-# %%
-pd.set_option("display.max_rows", 50)
-
-# %%
-# pd.options.display.max_rows = 30
-
-df_counts = df.groupby(["layer_index", "feature_index", "unique_index", "type"])[
-    "index"
-].count()
-df_counts.head(50)
-
-# %%
-df[df["neuron_index"] == 161]["label"].value_counts()
-
-
-# %%
-df.info(memory_usage="deep")
-
-
-# %%
-
-
-make_feature_id("CLIP_TINY", "NEURON", 1, "FC1", 102)
